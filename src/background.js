dcc479b0629bc47ce09969228f4f718bcb987c88
StartForKiller <jesussanz2003@gmail.com>
2024-09-08 01:23:28 +0200

    Now working on new V3 format

--- src/background.js
+++ src/background.js
@@ -17,34 +17,43 @@ function checkCookie(url, name) {
 
 //Send a basic request to find all the cookies
 const sendRequests = () => {
-    return jQuery.get('https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml');
+    return fetch('https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml', {
+        method: 'GET'
+    });
 };
 
 //Send a second request to extract the array of events
 const sendRequestsTwo = (source, view, submit) => {
     var body_payload= `javax.faces.partial.ajax=true&javax.faces.source=${source}&javax.faces.partial.execute=${source}&javax.faces.partial.render=${source}&${source}=${source}&${source}_start=${Date.now()}&${source}_end=${Date.now() + 21168000000}&${submit}_SUBMIT=1&javax.faces.ViewState=${view}`;
-    return jQuery.post({
-        url: 'https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml',
-        data: body_payload,
-        contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+    return fetch('https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: body_payload
     });
 };
 
 async function doTheWork() {
     var request = await sendRequests();
+    var requestText = await request.text();
 
     //Collect all the in-document cookies
-    var source = 'j_id' + (request.split('<div id="j_id')[1]).split('"')[0];
-    var viewstate = (request.split(':javax.faces.ViewState:1" value="')[1]).split('"')[0];
-    var submit = (request.split('" method="post" action="/serviciosacademicos/web/expedientes/calendario.xhtml"')[0]).split('j_id');
+    var source = 'j_id' + (requestText.split('<div id="j_id')[1]).split('"')[0];
+    var viewstate = (requestText.split(':javax.faces.ViewState:1" value="')[1]).split('"')[0];
+    var submit = (requestText.split('" method="post" action="/serviciosacademicos/web/expedientes/calendario.xhtml"')[0]).split('j_id');
     submit = 'j_id' + submit[submit.length - 1];
     viewstate = viewstate.replaceAll("+", "%2B");
 
     var response = await sendRequestsTwo(source, viewstate, submit);
 
     //Get and parse the JSON entries
-    var entry = jQuery(response).find('update').get(0).textContent;
-    var calendarJson = JSON.parse(entry);
+    var responseText = await response.text();
+    responseText = responseText.split('<![CDATA[')[1];
+    responseText = responseText.split('</update>')[0];
+    responseText = responseText.split(']]>')[0];
+
+    var calendarJson = JSON.parse(responseText);
 
     //Start creating the csv from the JSON
     var csvString = 'Asunto,Fecha de comienzo,Comienzo,Fecha de finalización,Finalización,Todo el dí­a,Reminder on/off,Reminder Date,Reminder Time,Meeting Organizer,Required Attendees,Optional Attendees,Recursos de la reuniÃƒÂ³n,Billing Information,Categories,Description,Location,Mileage,Priority,Private,Sensitivity,Show time as\n';
